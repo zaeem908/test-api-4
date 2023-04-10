bc25d149011d835ebc7ffa408bf4301fba362dd7
Zaeem Khan <zaeembaloch1198@gmail.com>
2023-04-10 12:05:39 +0500

    added files

--- queries.js
+++ queries.js
@@ -2,7 +2,7 @@ const {Client} = require('pg');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const sgMail = require('@sendgrid/mail')
-let loggedIn = false;
+let userToken ;
   sgMail.setApiKey('123')
    
 const client = new Client({
@@ -35,10 +35,14 @@ function validPassword(password) {
   }
 }
 
+function createToken(data) {
+  const token = jwt.sign({data:data},'secretpassword')
+  return token
+}  
+
 const logIn = async (request, response) => {
    try {
       const {email,password} = request.body
-      const token = jwt.sign({email:email,password:password},'secretkey')
 
 
       const result = await client.query(`SELECT email,password FROM users3 WHERE email = $1`,[email]);
@@ -56,9 +60,10 @@ const logIn = async (request, response) => {
      if(user) {
         const passwordMatch = await bcrypt.compare(password,user.password) 
         if(passwordMatch) {
-         response.send(token)
+         userToken = createToken(email)
+         response.send(userToken)
          loggedIn = true;
-         console.log('login succesful')
+         console.log('login succesful') 
        }else{
         console.log('password not matched')
        }
@@ -90,17 +95,18 @@ const logIn = async (request, response) => {
   }catch(err){
     console.log(err)
     response.status(500).send('no such email in database')
-  }
-  };
+  } 
+  }; 
 
   const addSubject = async (request,response) => {
     try{
-      const {subject} = request.body
-      if(loggedIn) {
+      const {token,subject} = request.body
+      const verified = jwt.verify(token,'secretpassword')
+      if(verified) {
         client.query(`INSERT INTO subjects2 (subject) VALUES ($1)`,[subject])
         response.send(`${subject} added to subjects`)
       }
-      if(!loggedIn) {
+      if(!verified) {
         response.send('user not logged in!')
       }
 
